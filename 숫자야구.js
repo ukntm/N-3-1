8b7d8612af2b2e0d38a0389dcc6881f4655dbffb
ukntm <aszxz174@naver.com>
2023-04-06 17:03:59 +0900

    숫자 비교 함수, 게임실행로직 추가

--- 숫자야구.js
+++ 숫자야구.js
@@ -1,26 +1,47 @@
-// 숫자야구
-
-// - 컴퓨터는 0과 9 사이의 서로 다른 숫자 3개를 무작위로 뽑습니다. (ex) 123, 759
-// - 사용자는 컴퓨터가 뽑은 숫자를 맞추기 위해 시도합니다.
-// - 컴퓨터는 사용자가 입력한 세자리 숫자에 대해서, 아래의 규칙대로 스트라이크(S)와 볼(B)를 알려줍니다.
-//     - 숫자의 값과 위치가 모두 일치하면 S
-//     - 숫자의 값은 일치하지만 위치가 틀렸으면 B
-// - 기회는 무제한이며, 몇번의 시도 후에 맞췄는지 기록됩니다.
-// - 숫자 3개를 모두 맞춘 경우, 게임을 종료합니다.
+// 숫자 야구
 
 // 랜덤 숫자 생성 로직
+const baseball = [];
 
-let NumBaseball = function() {
-    const baseball = [];
-    for (let i =0; i < 3; i++) {
-        baseball.push(Math.floor(Math.random()*(9)+0)+"");
-        if (baseball[i] === baseball[i-1] || baseball[i] === baseball[i-2] ) {
-            baseball.pop(i);
-            i -= 1;
-        }
+// 랜덤 숫자 생성 로직
+let NumBaseball = function () {
+  for (let i = 0; i < 3; i++) {
+    baseball.push(Math.floor(Math.random() * 9 + 0) + "");
+    if (baseball[i] === baseball[i - 1] || baseball[i] === baseball[i - 2]) {
+      baseball.pop(i);
+      i -= 1;
     }
-    return baseball.join('');
+  }
+  return baseball;
+};
+NumBaseball();
+
+// 숫자 비교 함수
+let play = function (input) {
+  let ball = 0;
+  let strike = 0;
+  let useInput = input.split('');
+  for (i = 0; i < 3; i++) {
+    const index = useInput.indexOf(baseball[i]);
+    if (index > -1) {
+      if (index === i) {
+        strike++;
+      } else {
+        ball++;
+      }
+    }
+  }
+  strike === 3 ? console.log("홈런") : console.log(strike+'S'+ball+'B')
+};
+
+// 게임 실행 로직
+let guessCount = 0;
+while (true) {
+  const input = prompt("3자리 숫자를 입력하세요");
+  const result = play(input);
+  guessCount++;
+  console.log(guessCount + "번째 시도");
+  if(input === String(baseball.join())) {
+    break;
+  } 
 }
-
-
-
